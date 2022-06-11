f0fe1b27436567b1c94db245c462dab910dc1b87
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-06-12 00:00:50 +0800

    feat: server pushes init pos to client; preserve game state when player accidentally disconnects

--- server.py
+++ server.py
@@ -15,44 +15,50 @@ except socket.error as e:
     print(str(e))
 
 s.listen(2)
-print("Waiting for a connection")
+print("waiting for a connection")
 
-currentId = "0"
+currentId = 0
 pos = ["0:50,50", "1:100,100"]
 
 
 def threaded_client(conn):
     global currentId, pos
-    conn.send(str.encode(currentId))
-    currentId = "1"
+    conn.send(str(currentId).encode())
+    print(f'assigned id {currentId}')
+    currentId += 1
     while True:
         try:
             data = conn.recv(2048)
-            reply = data.decode('utf-8')
             if not data:
-                conn.send(str.encode("Goodbye"))
+                conn.send(str.encode("goodbye"))
                 break
             else:
-                print("Received: " + reply)
-                arr = reply.split(":")
-                player_id = int(arr[0])
-                pos[player_id] = reply
+                this_state = data.decode('utf-8')
+                print("received: " + this_state)
+                arr = this_state.split(":")
+                this_id = int(arr[0])
 
-                nid = 1 if player_id == 0 else 1
-                reply = pos[nid][:]
-                print("Sending: " + reply)
+                is_fetching_init_pos = arr[1] == ''
+                if is_fetching_init_pos:
+                    reply = pos[this_id]
+                else:
+                    pos[this_id] = this_state
+                    another_id = 1 if this_id == 0 else 0
+                    reply = pos[another_id][:]
+
+                print("sending: " + reply)
 
             conn.sendall(str.encode(reply))
         except:
             break
 
-    print("Connection Closed")
+    print("connection closed")
+    currentId -= 1
     conn.close()
 
 
 if __name__ == '__main__':
     while True:
         conn, addr = s.accept()
-        print("Connected to: ", addr)
-
+        print("connected to: ", addr)
         start_new_thread(threaded_client, (conn,))
